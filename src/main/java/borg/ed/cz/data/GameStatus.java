01fecff053e916c53a23274bd3772e6117286317
Boris Günther <boris.guenther@redteclab.com>
2019-02-12 17:03:32 +0100

    Changed how targeting works

--- src/main/java/borg/ed/cz/data/GameStatus.java
+++ src/main/java/borg/ed/cz/data/GameStatus.java
@@ -17,5 +17,31 @@ public class GameStatus {
 	private boolean fighterRebuilt = true;
 	private ScannedShip targetedShip = null;
 	private List<ScannedShip> scannedShips = new ArrayList<>();
+	
+	/**
+	 * Check for how long the current target is selected.
+	 * 
+	 * @return <code>Long.MAX_VALUE</code> if we do not have a target.
+	 */
+	public long currentTargetMillis() {
+	    if(this.targetedShip == null) {
+	        return Long.MAX_VALUE;
+	    } else {
+	        return System.currentTimeMillis() - this.targetedShip.getLastSeen();
+	    }
+	}
+	
+	/**
+	 * Get the scan stage of the current target.
+	 * 
+	 * @return 0 if we do not have a target.
+	 */
+	public int currentTargetScanStage() {
+	    if(this.targetedShip == null) {
+	        return 0;
+	    } else {
+	        return this.targetedShip.getScanStage();
+	    }
+	}
 
 }
--- src/main/java/borg/ed/cz/tasks/SelectHighestThreatTask.java
+++ src/main/java/borg/ed/cz/tasks/SelectHighestThreatTask.java
@@ -1,5 +1,6 @@
 package borg.ed.cz.tasks;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +14,41 @@ import borg.ed.galaxy.robot.ShipControl;
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SelectHighestThreatTask extends Thread {
 
-	@Autowired
-	private ShipControl shipControl = null;
+    @Autowired
+    private ShipControl shipControl = null;
 
-	private final Random random = new Random();
+    private final Random random = new Random();
 
-	@Override
-	public void run() {
-		try {
-			Thread.sleep(this.shipControl.selectHighestThreat() + 50 + this.random.nextInt(100));
-		} catch (InterruptedException e) {
-			// Quit
-		}
-	}
+    private static final LinkedList<Long> lastExecutionTimes = new LinkedList<>();
+
+    @Override
+    public void run() {
+        try {
+            if (isNextExecutionAllowed()) {
+                synchronized (lastExecutionTimes) {
+                    lastExecutionTimes.addLast(System.currentTimeMillis());
+                }
+                Thread.sleep(this.shipControl.selectHighestThreat() + 50 + this.random.nextInt(100));
+            }
+        } catch (InterruptedException e) {
+            // Quit
+        }
+    }
+
+    public static boolean isNextExecutionAllowed() {
+        // Max 20 per minute AND max 1 per 500 ms
+        final long now = System.currentTimeMillis();
+        final long nowMinusOneMinute = now - 60_000L;
+        synchronized (lastExecutionTimes) {
+            lastExecutionTimes.removeIf(ts -> ts < nowMinusOneMinute);
+            if (lastExecutionTimes.isEmpty()) {
+                return true;
+            } else if (now - lastExecutionTimes.getLast() < 500) {
+                return false;
+            } else {
+                return lastExecutionTimes.size() < 20;
+            }
+        }
+    }
 
 }
